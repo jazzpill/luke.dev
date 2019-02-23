a911b48d216803f18487954e292d0bb67b49e339
Luke Martin <luke@lukemartin.me>
2019-02-23 21:41:22 +0000

    Anim

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,52 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+import { useSpring, animated } from 'react-spring';
 import SEO from '../components/seo';
 import { GlobalStyle } from '../components/GlobalStyle';
 
-const IndexPage = () => (
-	<>
-		<GlobalStyle />
-		<SEO title="Hi 👋" keywords={[]} />
-		<div class="content">
-			<h1>Hello.</h1>
+const IndexPage = () => {
+	const props = useSpring({
+		r: 0,
+		from: { r: 90 },
+		config: {
+			mass: 1,
+			tension: 330,
+			velocity: 2,
+			friction: 11,
+		},
+	});
 
-			<p>
-				My name is Luke. I'm a Lead Engineer at{' '}
-				<a href="http://fathomlondon.com">Fathom</a>.
-			</p>
-		</div>
-	</>
-);
+	return (
+		<>
+			<GlobalStyle />
+			<SEO title="Hi 👋" keywords={[]} />
+			<div css={{ padding: 20 }}>
+				<h1 css={{ fontSize: 32 }}>
+					Hi{' '}
+					<animated.span
+						role="img"
+						aria-label="waving emoji"
+						style={{
+							display: 'inline-block',
+							transformOrigin: 'center',
+							transform: props.r.interpolate(x => `rotate(${x}deg)`),
+						}}
+					>
+						👋
+					</animated.span>
+				</h1>
+
+				<p
+					css={{
+						fontSize: 24,
+					}}
+				>
+					My name is Luke. I'm a Lead Engineer at{' '}
+					<a href="https://fath.om">Fathom</a>.
+				</p>
+			</div>
+		</>
+	);
+};
 
 export default IndexPage;
