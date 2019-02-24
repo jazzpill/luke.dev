3ef84c0a0430d8fbca4222a618d9cc88c1851d86
Luke Martin <luke@lukemartin.me>
2019-02-24 12:05:01 +0000

    Finished?

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 import normalize from 'normalize.css';
 
+const COLOR = `hsla(190, 40%, 23%, 1)`;
+const COLOR_HOVER = `hsla(190, 40%, 10%, 1)`;
+
 export const GlobalStyle = createGlobalStyle`
 	${normalize};
 
@@ -18,5 +21,33 @@ export const GlobalStyle = createGlobalStyle`
 		font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial,
 			sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
 		font-size: 0.625rem;
+		color: ${COLOR};
+	}
+
+	html, body, #___gatsby, #___gatsby > div {
+		height: 100%;
+	}
+
+	a, a:link, a:active, a:visited {
+		color: ${COLOR};
+		text-decoration: none;
+		position: relative;
+
+		p &:after {
+			content: "";
+			position: absolute;
+			bottom: -2px;
+			left: 0;
+			border-bottom: 2px solid ${COLOR};
+			width: 100%;
+		}
+	}
+
+	a:hover {
+		color: ${COLOR_HOVER};
+	}
+
+	svg {
+		fill: ${COLOR};
 	}
 `;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+// eslint-disable-next-line
 import styled from 'styled-components/macro';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, useTransition, animated } from 'react-spring';
 import SEO from '../components/seo';
 import { GlobalStyle } from '../components/GlobalStyle';
+import GitHubIcon from '../images/github.svg';
+import TwitterIcon from '../images/twitter.svg';
+import LastFMIcon from '../images/lastfm.svg';
+
+const SOCIAL = [
+	{
+		icon: GitHubIcon,
+		label: 'GitHub',
+		url: 'https://github.com/lukemartin',
+	},
+	{
+		icon: TwitterIcon,
+		label: 'Twitter',
+		url: 'https://twitter.com/luke_is',
+	},
+	{
+		icon: LastFMIcon,
+		label: 'last.fm',
+		url: 'https://www.last.fm/user/lukus',
+	},
+];
 
 const IndexPage = () => {
-	const props = useSpring({
+	const handProps = useSpring({
 		r: 0,
 		from: { r: 90 },
 		config: {
@@ -15,21 +37,40 @@ const IndexPage = () => {
 			friction: 11,
 		},
 	});
+	const list = useTransition(SOCIAL, item => item.label, {
+		unique: true,
+		enter: {
+			opacity: 1,
+			transform: 'translateX(0)',
+		},
+		from: {
+			opacity: 0,
+			transform: 'translateX(-10px)',
+		},
+		trail: 200 / 3,
+	});
 
 	return (
 		<>
 			<GlobalStyle />
 			<SEO title="Hi 👋" keywords={[]} />
-			<div css={{ padding: 20 }}>
-				<h1 css={{ fontSize: 32 }}>
-					Hi{' '}
+			<div
+				css={{
+					padding: 40,
+					display: 'flex',
+					flexDirection: 'column',
+					height: '100%',
+				}}
+			>
+				<h1 css={{ fontSize: 36 }}>
+					Hi {/*eslint-disable-next-line*/}
 					<animated.span
 						role="img"
 						aria-label="waving emoji"
 						style={{
 							display: 'inline-block',
 							transformOrigin: 'center',
-							transform: props.r.interpolate(x => `rotate(${x}deg)`),
+							transform: handProps.r.interpolate(x => `rotate(${x}deg)`),
 						}}
 					>
 						👋
@@ -39,11 +80,45 @@ const IndexPage = () => {
 				<p
 					css={{
 						fontSize: 24,
+						flex: '1 1 auto',
 					}}
 				>
 					My name is Luke. I'm a Lead Engineer at{' '}
 					<a href="https://fath.om">Fathom</a>.
 				</p>
+
+				<ul
+					css={{
+						listStyleType: 'none',
+						margin: '30px 0 0',
+						padding: 0,
+						flex: 0,
+					}}
+				>
+					{list.map(({ item: { icon: Icon, label, url }, props, key }) => (
+						<animated.li
+							key={key}
+							style={{
+								display: 'flex',
+								flexDirection: 'row',
+								justifyContent: 'flex-start',
+								alignItems: 'center',
+								marginBottom: 10,
+								...props,
+							}}
+						>
+							<Icon />
+							<a
+								href={url}
+								target="_blank"
+								rel="noopener noreferrer"
+								css={{ marginLeft: 10, fontSize: 14 }}
+							>
+								{label}
+							</a>
+						</animated.li>
+					))}
+				</ul>
 			</div>
 		</>
 	);
