168a349fa5a7b6e23a5938c5870e5404715d6bbb
Luke Martin <me@luke.dev>
2021-03-13 22:00:32 +0000

    Cheeky animations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,7 +25,7 @@
 	description="Personal web site of Luke Martin, a Lead UI Developer from London"
 />
 
-<h1>Hi 👋</h1>
+<h1>Hi <span>👋</span></h1>
 
 <p>My name is Luke. I'm a Lead UI Developer.</p>
 
@@ -46,16 +46,55 @@
 </ul>
 
 <style>
+	@keyframes slideIn {
+		from {
+			opacity: 0;
+			transform: translateY(100px);
+		}
+
+		to {
+			opacity: 1;
+			transform: translateY(0);
+		}
+	}
+
+	@keyframes wave {
+		0% {
+			transform: rotate(0deg);
+		}
+		25% {
+			transform: rotate(45deg);
+		}
+		50% {
+			transform: rotate(45deg);
+		}
+		75% {
+			transform: rotate(-45deg);
+		}
+		100% {
+			transform: rotate(0deg);
+		}
+	}
+
 	h1 {
+		animation: slideIn 500ms cubic-bezier(0.17, 0.67, 0.14, 0.98) 200ms both;
 		font-size: 36px;
 	}
 
+	h1 span {
+		animation: wave 500ms ease 1.2s both;
+		transform-origin: 20px;
+		display: inline-block;
+	}
+
 	p {
+		animation: slideIn 500ms cubic-bezier(0.17, 0.67, 0.14, 0.98) 400ms both;
 		font-size: 24px;
 		flex: 1 1 auto;
 	}
 
 	ul {
+		animation: slideIn 500ms cubic-bezier(0.17, 0.67, 0.14, 0.98) 600ms both;
 		list-style-type: none;
 		margin: 30px 0 0;
 		padding: 0;
